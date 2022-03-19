02908d1ee7e553e745387693b434879ca44f6675
devendra prasad <devendraprasad1984@gmail.com>
2022-03-19 16:32:27 +0530

    performance boost

--- src/components/events/event.tsx
+++ src/components/events/event.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 import {EventType, MarketType} from "../../config/app-data-types";
 import Market from "./market";
 import {Card} from "antd";
@@ -7,16 +7,16 @@ type Props = {
     event: EventType
 }
 
-const Event: React.FC<Props> = (props: any): JSX.Element => {
+const display = (event: any): JSX.Element[] => {
+    return event.markets.map((market: MarketType) => <Market key={market.id} data={market}/>)
+}
+
+const Event: React.FC<Props> = (props): JSX.Element => {
     const {event} = props
-
-    const display = (): JSX.Element[] => {
-        return event.markets.map((market: MarketType) => <Market key={market.id} data={market}/>)
-    }
-
-    const title=<h2>{event.name}</h2>
+    const displayEventFromCache = useMemo(() => display(event), [event])
+    const title = <h2>{event.name}</h2>
     return <Card title={title} bordered={true}>
-        {display()}
+        {displayEventFromCache}
     </Card>
 }
 export default React.memo(Event)
--- src/components/events/eventList.tsx
+++ src/components/events/eventList.tsx
@@ -1,20 +1,23 @@
-import React, {useCallback} from 'react'
+import React, {useMemo} from 'react'
 import {useSelector} from "react-redux";
 import {RootState} from "../../_redux/store";
 import {EventType, ResponseType} from "../../config/app-data-types";
 import Event from "./event";
 
 type Props = {}
+const displayEventCards = (eventData: any) => {
+    //logical flow=>events->markets->selections->selection
+    return eventData.map((event: EventType) => {
+        if (event.markets.length === 0) return null
+        return <Event key={event.id} event={event}/>
+    })
+}
+
+
 const EventList: React.FC<Props> = (props): JSX.Element => {
     const {} = props
     const eventData: ResponseType = useSelector((_: RootState) => _.mockdata)
-    const displayEventCards =useCallback((): any => {
-        //logical flow=>events->markets->selections->selection
-        return eventData.map((event: EventType) => {
-            if (event.markets.length === 0) return null
-            return <Event key={event.id} event={event} />
-        })
-    },[eventData])
-    return <>{displayEventCards()}</>
+    const displayEventFromCache = useMemo(() => displayEventCards(eventData), [eventData])
+    return <>{displayEventFromCache}</>
 }
 export default React.memo(EventList)
